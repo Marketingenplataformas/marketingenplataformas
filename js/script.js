af4cd8c617a1d92b537db60b1e0ab123228bd8f8
Marketingenplataformas <daminontal0@gmail.com>
2025-01-17 16:48:29 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// script.js (Opcional)
+document.addEventListener('DOMContentLoaded', () => {
+  const whatsappButton = document.querySelector('.btn');
+  whatsappButton.addEventListener('click', () => {
+    console.log('El usuario hizo clic en el botón de WhatsApp');
+  });
+});
